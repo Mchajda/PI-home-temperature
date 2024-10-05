4b00a8c1e7d4f7c327f5af6f0cdf15481b314cc4
Maciej <maciej.chajda@gmail.com>
2024-10-05 19:50:24 +0200

    displaying all 4 rooms

--- zero/temperature-dashboard/src/App.tsx
+++ zero/temperature-dashboard/src/App.tsx
@@ -1,10 +1,33 @@
 import { RoomChart } from "./components/RoomChart";
+import { Container, Card, CardContent, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import "./App.css";
 
 function App() {
-  
   return (
-    <RoomChart roomName={"outside"} />
+    // <Container sx={{ '> *': {my: 2} }}>
+    //   <RoomChart roomName={"outside"} />
+    //   <RoomChart roomName={"bedroom"} />
+    //   <RoomChart roomName={"living_room"} />
+    //   <RoomChart roomName={"office"} />
+    // </Container>
+
+    <Container>
+      <Grid container spacing={2}>
+        <Grid size={6}>
+          <RoomChart roomName={"outside"} />
+        </Grid>
+        <Grid size={6}>
+          <RoomChart roomName={"bedroom"} />
+        </Grid>
+        <Grid size={6}>
+          <RoomChart roomName={"living_room"} />
+        </Grid>
+        <Grid size={6}>
+          <RoomChart roomName={"office"} />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
--- zero/temperature-dashboard/src/components/RoomChart.tsx
+++ zero/temperature-dashboard/src/components/RoomChart.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  useQuery,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { Container, Card, CardContent, Typography } from "@mui/material";
 import {
   LineChart,
@@ -17,8 +13,8 @@ import {
 } from "recharts";
 
 interface RoomChartProps {
-    roomName: string;
-  }
+  roomName: string;
+}
 
 // Define the interface for the API response
 interface DataProps {
@@ -41,7 +37,7 @@ const fetchTemperatureData = async (roomName: string): Promise<DataProps[]> => {
   }));
 };
 
-export const RoomChart: React.FC<RoomChartProps> = ({roomName}) => {
+export const RoomChart: React.FC<RoomChartProps> = ({ roomName }) => {
   const { data, error, isLoading } = useQuery<DataProps[], Error>({
     queryKey: ["temperatureData", roomName] as const,
     queryFn: () => fetchTemperatureData(roomName),
@@ -56,43 +52,41 @@ export const RoomChart: React.FC<RoomChartProps> = ({roomName}) => {
   }
 
   return (
-    <Container>
-      <Card sx={{ height: 600 }}>
-        <CardContent sx={{ height: 400 }}>
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              width={500}
-              height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="date" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="temperature"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-              <Line type="monotone" dataKey="humidity" stroke="#82ca9d" />
-            </LineChart>
-          </ResponsiveContainer>
-          <Typography gutterBottom variant="h5" component="div">
-            Temperature and Humidity Data
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            This chart displays the temperature and humidity data over time.
-          </Typography>
-        </CardContent>
-      </Card>
-    </Container>
+    <Card sx={{ height: 450 }}>
+      <CardContent sx={{ height: 350 }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="temperature"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            <Line type="monotone" dataKey="humidity" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
+        <Typography gutterBottom variant="h5" component="div">
+          Temperature and Humidity Data in {roomName}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          This chart displays the temperature and humidity data over time.
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
